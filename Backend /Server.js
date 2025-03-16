23ed93ca1449416f9a0cee1ce40f35780b3c0eb3
Stack <stack@Roopeshs-MacBook-Air.local>
2025-03-16 12:19:38 +0530

    Controllers Added

--- Backend /Server.js
+++ Backend /Server.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
+
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
